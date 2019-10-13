6aad7407e0a968277f7e051d9e299a957bec5f17
SanjanaKumar07 <56474199+SanjanaKumar07@users.noreply.github.com>
2019-10-13 12:04:06 +0530

    Update reverse.java

--- java/reverse.java
+++ java/reverse.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
-class Reverse
+public class Main
 {
 	public static void main(String args[])
 	{
+	  
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter a number : ");
-		int x = sc.nextInt();
-		int sum=0;
-		while(x!=0)
-		{
-			sum=sum*10+(x%10);
-			x=x/10;
-		}
-		System.out.println("Reverse of number is "+sum);
+		System.out.println("enter a string : ");
+		String n = sc.nextLine();
+	
+	 StringBuffer sb = new StringBuffer(n);
+	 sb.reverse();
+	 System.out.println("Reversed string is " +sb);
 	}
 }
